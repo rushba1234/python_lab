39ea3a97d01e1eaf4ce12a3e87f2d12ec3196bf6
rushba1234 <rushbanunu@gmail.com>
2025-10-21 15:12:58 +0530

    done

---
+++ swap.py
@@ -0,0 +1,10 @@
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+
+new_str1 = str1[0] + str2[1] + str1[2:]
+new_str2 = str2[0] + str1[1] + str2[2:]
+
+str3 = new_str1 + "." + new_str2
+
+print("Result: " + str3)
+
