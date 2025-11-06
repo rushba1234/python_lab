f2bdb9b74f027d83e505a086ec45d1a4672b4162
rushba1234 <rushbanunu@gmail.com>
2025-11-06 10:22:31 +0530

    done

---
+++ lamda.py
@@ -0,0 +1,12 @@
+area_square = lambda side:side * side
+area_rectangle = lambda length, width: length * width
+area_triangle = lambda base, height: 0.5 * base * height
+side = float(input("Enter side of square: "))
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter width of rectangle: "))
+base = float(input("Enter base of triangle: "))
+height = float(input("Enter height of triangle: "))
+print("Area of square:", area_square(side))
+print("Area of rectangle:", area_rectangle(length, width))
+print("Area of triangle:", area_triangle(base, height))
+
