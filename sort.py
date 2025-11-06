9bb273068f44e8a77ce677e388a88d1ad8cc3a4c
rushba1234 <rushbanunu@gmail.com>
2025-11-06 10:14:06 +0530

    done
    Fix syntax errors and improve dictionary sorting output.

---
+++ sort.py
@@ -0,0 +1,8 @@
+my_dict = {input("key:"):
+           int(input("value:"))for_in
+           range(int(input("Number of items:")))}
+           print(Ascending:",
+           dict(sorted(my_dict.items())))
+           print("Descending:",
+           dict(sorted(my_dict.items(),
+           reverse=true)))
